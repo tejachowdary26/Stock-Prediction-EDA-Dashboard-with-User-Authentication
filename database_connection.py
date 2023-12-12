2ca5703e94a7e9bb07f80622b336923d67fe78d5
EDA_Project <145912795+tejachowdary26@users.noreply.github.com>
2023-12-12 10:00:55 +0000

    Update database_connection.py

--- database_connection.py
+++ database_connection.py
@@ -106,15 +106,48 @@ def verify_user(token):
         if connection.is_connected():
             cursor.close()
             connection.close()
+        
+def get_stock_data(connection, symbol):
+    try:
+        cursor = connection.cursor(buffered=True)
+        query = "SELECT date, open, high, low, close, volume FROM stock_table WHERE symbol = %s"
+        cursor.execute(query, (symbol,))
+        data = cursor.fetchall()
 
+        if data:
+            df = pd.DataFrame(data, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
+            df.set_index('date', inplace=True)
+            return df
+        else:
+            return None
+
+    except Error as e:
+        print(f"Error fetching stock data: {e}")
+        return None
+    finally:
+        if connection.is_connected():
+            cursor.close()
 
 def download_and_store_stock_data(connection, symbol, start_date, end_date):
-    data = yf.download(symbol, start=start_date, end=end_date)
-    cursor = connection.cursor()
-    for index, row in data.iterrows():
-        cursor.execute(
-            "INSERT INTO stock_table (symbol, date, open, high, low, close, volume) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-            (symbol, index.strftime('%Y-%m-%d'), row['Open'], row['High'], row['Low'], row['Close'], row['Volume'])
-        )
-    connection.commit()
+    try:
+        data = yf.download(symbol, start=start_date, end=end_date)
+        if data.empty:
+            print(f"No data found for {symbol} in the given date range.")
+            return False
 
+        cursor = connection.cursor()
+        for index, row in data.iterrows():
+            cursor.execute(
+                "INSERT INTO stock_table (symbol, date, open, high, low, close, volume) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                (symbol, index.strftime('%Y-%m-%d'), row['Open'], row['High'], row['Low'], row['Close'], row['Volume'])
+            )
+        connection.commit()
+        print("Stock data successfully stored in the database.")
+        return True
+
+    except Error as e:
+        print(f"Error downloading and storing stock data: {e}")
+        return False
+    finally:
+        if connection.is_connected():
+            cursor.close()
