2f97490d33370fb53b8472b6a2dd5533eeaa774c
EDA_Project <145912795+tejachowdary26@users.noreply.github.com>
2023-12-12 09:21:30 +0000

    Update email_service.py

--- email_service.py
+++ email_service.py
@@ -1,15 +1,56 @@
+import smtplib
 import secrets
-def send_verification_email(email, username, smtp_details):
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+SMTP_SERVER = "your_smtp_server"
+SMTP_PORT = 587
+SMTP_USERNAME = "your_email_address"
+SMTP_PASSWORD = "your_email_password"
+
+def send_email(subject, body, recipient, smtp_details):
+    msg = MIMEMultipart()
+    msg['From'] = smtp_details['username']
+    msg['To'] = recipient
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP(smtp_details['server'], smtp_details['port'])
+    server.starttls()
+    server.login(smtp_details['username'], smtp_details['password'])
+    text = msg.as_string()
+    server.sendmail(smtp_details['username'], recipient, text)
+    server.quit()
+
+def generate_verification_token():
+    return secrets.token_urlsafe(16)
+
+def generate_verification_link(username):
+    token = generate_verification_token()
+    # Store the token in the database for the user
+    database_connection.add_verification_token(username, token)
+    return f"http://yourdomain.com/verify?token={token}"
+
+def send_verification_email(email, username):
+    smtp_details = {
+        "server": SMTP_SERVER,
+        "port": SMTP_PORT,
+        "username": SMTP_USERNAME,
+        "password": SMTP_PASSWORD
+    }
     verification_link = generate_verification_link(username)
     subject = "Verify Your Email"
     body = f"Please click on the link to verify your account: {verification_link}"
-    # send_email(subject, body, email, **smtp_details)
-    # For now, let's just print it (Replace with actual email sending in production)
-    print(f"Verification email to {email}: {verification_link}")
-
+    send_email(subject, body, email, smtp_details)
 
 def send_admin_creation_request(admin_email, new_admin_data):
-    # Simulate sending an email to the admin
-    print(f"Admin Creation Request to {admin_email}:")
-    print(f"New Admin Data: Username - {new_admin_data['username']}, Email - {new_admin_data['email']}")
-    print("Please create an admin account for this user.")
+    smtp_details = {
+        "server": SMTP_SERVER,
+        "port": SMTP_PORT,
+        "username": SMTP_USERNAME,
+        "password": SMTP_PASSWORD
+    }
+    subject = "Admin Account Creation Request"
+    body = f"New Admin Data: Username - {new_admin_data['username']}, Email - {new_admin_data['email']}\nPlease create an admin account for this user."
+    send_email(subject, body, admin_email, smtp_details)
