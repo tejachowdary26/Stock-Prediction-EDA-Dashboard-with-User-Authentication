6a4f2d05d5c8cb6722ab5af242dcc363055952db
EDA_Project <145912795+tejachowdary26@users.noreply.github.com>
2023-12-12 09:57:30 +0000

    Update home.py

--- home.py
+++ home.py
@@ -4,15 +4,55 @@ import streamlit as st
 import plotly.graph_objs as go
 from database_connection import create_connection, check_user
 from authentication import hash_password
+#import pandas as pd
+#import streamlit as st
+#import plotly.graph_objs as go
+from database_connection import create_connection, check_user, get_stock_data, download_and_store_stock_data
+# from authentication import hash_password
 
+
+# Function to get the content of a file from a URL
 def get_file_content_as_string(path):
-    url = 'https://raw.githubusercontent.com/Lakshya-Ag/Streamlit-Dashboard/master/' + path
-    response = urllib.request.urlopen(url)
-    return response.read().decode("utf-8")
+    try:
+        url = 'https://raw.githubusercontent.com/Lakshya-Ag/Streamlit-Dashboard/master/' + path
+        response = urllib.request.urlopen(url)
+        return response.read().decode("utf-8")
+    except Exception as e:
+        st.error("Error fetching file content: " + str(e))
+        return ""
 
-def main():
+# Function to display stock data
+def display_stock_data(stock_symbol):
     connection = create_connection()
+    if not connection:
+        st.error("Failed to connect to the database.")
+        return
+
+    stock_data = get_stock_data(connection, stock_symbol)
+
+    if stock_data:
+        # Assuming stock_data is a DataFrame
+        fig = go.Figure(data=[go.Candlestick(x=stock_data.index,
+                                             open=stock_data['Open'],
+                                             high=stock_data['High'],
+                                             low=stock_data['Low'],
+                                             close=stock_data['Close'])])
+        fig.update_layout(title=f'{stock_symbol} Stock Data', xaxis_rangeslider_visible=False)
+        st.plotly_chart(fig)
+    else:
+        # Prompt user for new input
+        new_stock_symbol = st.text_input("Enter Stock Symbol", value="AAPL")
+        start_date = st.date_input("Start Date", value=pd.to_datetime("2023-01-01"))
+        end_date = st.date_input("End Date", value=pd.to_datetime("2023-12-31"))
+        
+        if st.button("Fetch and Store Data"):
+            download_and_store_stock_data(connection, new_stock_symbol, start_date, end_date)
+            st.success("Data downloaded and stored in the database.")
+
+# Main function
+def main():
     st.sidebar.title("User Authentication")
+    connection = create_connection()
 
     if 'logged_in' not in st.session_state:
         st.session_state['logged_in'] = False
@@ -20,47 +60,20 @@ def main():
     if not st.session_state['logged_in']:
         username = st.sidebar.text_input("Username")
         password = st.sidebar.text_input("Password", type='password')
+
         if st.sidebar.button("Login"):
             if check_user(connection, username, hash_password(password)):
                 st.session_state['logged_in'] = True
                 st.session_state['username'] = username
-                st.sidebar.success("Logged In Successfully as " + username)
+                st.sidebar.success(f"Logged In Successfully as {username}")
             else:
                 st.sidebar.error("Incorrect Username or Password")
 
     if st.session_state['logged_in']:
-        st.sidebar.success("Logged In as " + st.session_state['username'])
-        box = st.selectbox("Information", ["Stock Market Info", "Covid-19 impact"])
-        if box == "Stock Market Info":
-            st.markdown(get_file_content_as_string("README.md"))
-        elif box == "Covid-19 impact":
-            display_covid_impact()
-
-def display_covid_impact():
-    st.header("Impact of COVID-19 on Indian Stock Market")
-    df1 = pd.read_csv("data nifty50.csv", index_col='Date', parse_dates=True)
-    fig = go.Figure(data=[go.Candlestick(x=df1.index,
-                                         open=df1['Open'],
-                                         high=df1['High'],
-                                         low=df1['Low'],
-                                         close=df1['Close'],
-                                         name='Market Data')])
-    fig.update_layout(
-        title='Nifty 50 volatility in 2020',
-        yaxis_title='Stock Price (in Rs.)', width=800, height=550)
-    fig.update_xaxes(rangeslider_visible=True,
-                     rangeselector=dict(
-                         buttons=list([
-                             dict(count=30, label="30D", step="day", stepmode="backward"),
-                             dict(count=60, label="60D", step="day", stepmode="backward"),
-                             dict(count=90, label="90D", step="day", stepmode="backward"),
-                             dict(count=180, label="180D", step="day", stepmode="backward"),
-                             dict(count=270, label="270D", step="day", stepmode="backward"),
-                             dict(step="all")
-                         ])
-                     ))
-    st.plotly_chart(fig)
-    st.markdown(get_file_content_as_string("Covid.md"))
+        st.sidebar.success(f"Logged In as {st.session_state['username']}")
+        st.title("Stock Market Dashboard")
+        stock_symbol = st.text_input("Enter Stock Symbol", value="AAPL")
+        display_stock_data(stock_symbol)
 
 if __name__ == "__main__":
     main()
